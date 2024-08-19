eebf9833181dc3df46d638d6dcf56cdc592cffb8
zentoo31 <zentoo31@gmail.com>
2024-08-19 00:30:55 -0500

    19/08/24

--- controllers/myAnimeListController.mjs
+++ controllers/myAnimeListController.mjs
@@ -1,18 +1,18 @@
-// import { fetchAnimeById } from "../services/myAnimeListService.mjs";
+import { fetchAnimeById } from "../services/myAnimeListService.mjs";
 import {getAnimeListRecent} from "../services/myAnimeListService.mjs";
 
-// export async function getAnimeById (req,res) {
-//     try {
-//         const { id } = req.params; 
-//         if (!id) {
-//           return res.status(400).json({ message: "ID es requerido" });
-//         }
-//         const data = await fetchAnimeById(id);
-//         res.json(data);
-//       } catch (error) {
-//         res.status(500).json({ message: error.message });
-//       }
-// }
+export async function getAnimeById (req,res) {
+    try {
+        const { id } = req.params; 
+        if (!id) {
+          return res.status(400).json({ message: "ID es requerido" });
+        }
+        const data = await fetchAnimeById(id);
+        res.json(data);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+}
 
 export async function getAllAnimes(req,res){
   try {
