608a57c010d6389dde936120e28761e0c4cd0e31
zentoo31 <zentoo31@gmail.com>
2024-08-25 23:27:30 -0500

    25/08/24

--- app.mjs
+++ app.mjs
@@ -1,19 +1,23 @@
+/* eslint-disable no-undef */
 import express from "express"
 import { conectarDB } from "./config/db.mjs";
 import router from "./routes/routes.mjs";
+import { configDotenv } from "dotenv";
 import cors  from 'cors'
 
+configDotenv({path: "variables.env"});
 
 const app = express();
+const port = process.env.PORT ?? 3000;
 
 conectarDB();
 app.use(cors());
-
+app.disable('x-powered-by');
 
 app.use(express.json());
 
 app.use('/api', router);
 
-app.listen(3000, () => {
-    console.log("El servidor esta funcionando correctamente");
+app.listen(port, () => {
+    console.log(`El servidor esta funcionando correctamente en: http://localhost:${port}`);
 });
--- routes/routes.mjs
+++ routes/routes.mjs
@@ -1,12 +1,12 @@
 import express from "express";
-import { crearManga, obtenerManga } from "../controllers/mangaController.mjs";
+import { crearManga } from "../controllers/mangaController.mjs";
 import { getAnimeById } from "../controllers/myAnimeListController.mjs";
 import { getAllAnimes } from "../controllers/myAnimeListController.mjs";
 import { getMangasTitle } from "../controllers/myAnimeListController.mjs";
+
 const router = express.Router();
 
 router.post('/', crearManga);
-router.get('/', obtenerManga);
 router.get('/manga/:id', getAnimeById);
 router.get('/manga/top/:parm', getAllAnimes);
 router.get('/manga/top', getAllAnimes);
